12870294088d4ce31f62022eee41054268aca6e8
anthony.inauen.2.ctr@us.af.mil <anthony.inauen.2.ctr@us.af.mil>
2025-04-24 14:04:43 -0400

    blank pages

---
+++ src/pages/PCR.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+
+const PCR = () => {
+    const user = useSelector((state) => state.auth.user);
+
+    return(
+        <div className='p-4'>
+            <h2>Welcome {user?.username || 'Guest'}</h2>
+        </div>
+    );
+};
+
+export default PCR;
